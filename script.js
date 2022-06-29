cad3b14bf3ac41d949efe348765c97f467443f65
Rabee <rabee@Talals-MacBook-Air.local>
2022-06-29 15:44:02 -0400

    add basic functions for library

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 let myLibrary = [];
 
-function Book() {
+function Book(title, author, pageNum) {
     //constructor
+    this.title = title;
+    this.author = author;
+    this.pageNum = pageNum;
+    this.isRead = false;
 }
 
 function addBookToLibrary() {
-    
+    let title = prompt("Enter title of book: ");
+    let author = prompt("Enter author of book: ");
+    let pageNum = prompt("Enter # of pages in book: ");
+    newBook = new Book(title, author, pageNum);
+    myLibrary.push(newBook);
 }
+addBookToLibrary();
